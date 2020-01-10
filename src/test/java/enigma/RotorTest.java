e2bed52730fe1ae6434d70b14a6b4ac7ec22240e
bp371587 <bp371587@D428-3.esirem-ad.ad.u-bourgogne.fr>
2020-01-10 11:03:41 +0100

    Commit avec commentaires et 1 test !

--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -5,22 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RotorTest {
-
-    int position;
+    
+    String[] rotorList = { "I", "II", "III", "IV", "V","VI", "VII", "VIII" };
+    Rotor rotor1 = Rotor.rotorFactory("E K M F L G D Q V Z N T O W Y H X U S P A I B R C J", "Q");
+    Rotor rotor8 = Rotor.rotorFactory("F K Q H T L X O C B J S P D Z R A M E W N I U Y G V","Z and M");
     int[] cipher = new int[26];
-    int[] bcipher = new int[26];
-    int notch1 = -1;
-    int notch2 = -1;
+    int position;
     
-    String config ="* B III IV I AXLE";
-    String msg1 = "FROM his shoulder Hiawatha";
-    String code1 = "HYIHBSBOEBTWJZSXMWQKYGWSFR";
-    String msg2 = "HYIHLBKOMLIUYDCMPPSFSZW";
-    String code2 = "FROMHISSHOULDERHIAWATHA";
-    
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    public void testAdvanceRotor(){
+        Rotor r = rotor1;
+        int pos = r.getPosition();
+        r.advance();
+        int expected = pos+1;
+        int actual = r.getPosition();
+        assertEquals(expected, actual);
+    }
 }
